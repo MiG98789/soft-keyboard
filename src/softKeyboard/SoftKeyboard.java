7da19d614cc6289afe36382b45cdd0611ab558e7
MiG98789 <gianmiguel98@gmail.com>
2017-08-16 01:38:05 +0800

    Modified version number

--- src/softKeyboard/SoftKeyboard.java
+++ src/softKeyboard/SoftKeyboard.java
@@ -8,7 +8,7 @@ package softKeyboard;
  * diseases that impairs hand and arm motor movements.</p>
  * 
  * @author Gian Miguel Sero Del Mundo, Jin Young Park
- * @version 1.1
+ * @version 1.2
  * @since 19/07/2017
  */
 public class SoftKeyboard {    
